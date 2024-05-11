8e53f06cf41a0562dc748a6187527f8151a419e6
yosephdev <yosephbet@gmail.com>
2024-05-11 07:40:28 +0200

    Update settings reference to 'bookdine.settings'

--- BookDine/wsgi.py
+++ BookDine/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'BookDine.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bookdine.settings')
 
 application = get_wsgi_application()
