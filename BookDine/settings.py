081c5ab4523cdc1027507994f61615685ad2d270
yosephdev <yosephbet@gmail.com>
2024-05-11 11:18:36 +0200

    Add crispy template packs to settings.py

--- BookDine/settings.py
+++ BookDine/settings.py
@@ -48,6 +48,9 @@ INSTALLED_APPS = [
     'booking_system',
 ]
 
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
