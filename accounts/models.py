9afbe9559362e71c6bd4c436d4d8332aa4e665ee
yosephdev <yosephbet@gmail.com>
2024-05-11 10:42:45 +0200

    Define custom user model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,26 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
+
+class CustomUser(AbstractUser):
+    """
+    Custom User model extending Django's AbstractUser.    
+    """
+    phone_number = models.CharField(max_length=15, blank=True, null=True)
+    date_of_birth = models.DateField(null=True, blank=True)
+    profile_picture = models.ImageField(
+        upload_to='profile_pictures', blank=True, null=True)
+
+    def __str__(self):
+        """
+        Returns a string representation of the user.
+        """
+        return self.username
+
+    class Meta:
+        """
+        Meta class for CustomUser model.
+        """
+        verbose_name = 'User'
+        verbose_name_plural = 'Users'
