f081c7af539621c23d57dad67428dbc865f04316
yosephdev <yosephbet@gmail.com>
2024-05-12 11:24:35 +0200

    Refactor views.py to include a home_view function

--- booking_system/views.py
+++ booking_system/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Restaurant, Reservation
 
 # Create your views here.
 
-def index(request):
-    return HttpResponse("Hello, BookDine!")
+def home_view(request):
+    # Get the featured restaurants
+    featured_restaurants = Restaurant.objects.order_by('-rating')[:5]
+
+    # Get the latest reservations
+    latest_reservations = Reservation.objects.order_by('-created_at')[:5]
+
+    context = {
+        'restaurants': featured_restaurants,
+        'latest_reservations': latest_reservations,
+    }
+    return render(request, 'booking_system/home.html', context)
