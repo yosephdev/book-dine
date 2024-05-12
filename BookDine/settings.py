2581020652057aed36a314d4b79931648da10829
yosephdev <yosephbet@gmail.com>
2024-05-12 15:10:26 +0200

    Configure template directory path in settings.py

--- BookDine/settings.py
+++ BookDine/settings.py
@@ -76,7 +76,7 @@ ROOT_URLCONF = 'BookDine.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [TEMPLATES_DIR],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
