06c035263269ff470bebd4b73c5c135a0efc10bb
yosephdev <yosephbet@gmail.com>
2024-05-14 18:24:54 +0200

    Correct import statement for views module

--- BookDine/urls.py
+++ BookDine/urls.py
@@ -19,7 +19,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
-from . import views as booking_views
+from booking_system import views as booking_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
