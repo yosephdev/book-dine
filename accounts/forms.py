822308a4ac36b8d83021a6d4c2b967b296dd81fc
yosephdev <yosephbet@gmail.com>
2024-05-16 09:53:43 +0200

    Add phone number validation to user forms in accounts/forms.py

--- accounts/forms.py
+++ accounts/forms.py
@@ -2,12 +2,22 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Layout, Field
+from django.core.validators import RegexValidator
 from .models import CustomUser
 
+
 class CustomUserCreationForm(UserCreationForm):
+    phone_number = forms.CharField(
+        max_length=15,
+        validators=[RegexValidator(
+            r'^\+?1?\d{9,15}$', 'Enter a valid phone number.')],
+        required=False
+    )
+
     class Meta(UserCreationForm.Meta):
         model = CustomUser
-        fields = UserCreationForm.Meta.fields + ('phone_number', 'date_of_birth')
+        fields = UserCreationForm.Meta.fields + \
+            ('phone_number', 'date_of_birth')
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -20,7 +30,15 @@ class CustomUserCreationForm(UserCreationForm):
             Field('date_of_birth', css_class='form-control'),
         )
 
+
 class CustomUserChangeForm(UserChangeForm):
+    phone_number = forms.CharField(
+        max_length=15,
+        validators=[RegexValidator(
+            r'^\+?1?\d{9,15}$', 'Enter a valid phone number.')],
+        required=False
+    )
+
     class Meta(UserChangeForm.Meta):
         model = CustomUser
         fields = '__all__'
