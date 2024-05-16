6899e17a9ace8ed04f1da483e73912165a8d8b40
yosephdev <yosephbet@gmail.com>
2024-05-16 12:35:47 +0200

    Enhance user model with phone number validation and internationalization

--- accounts/models.py
+++ accounts/models.py
@@ -1,27 +1,52 @@
 from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
+from django.core.validators import RegexValidator
 from django.db import models
+
 
 # Create your models here.
 
 class CustomUser(AbstractUser):
     """
-    Custom User model extending Django's AbstractUser.    
+    Custom User model extending Django's AbstractUser.
     """
-    phone_number = models.CharField(max_length=15, blank=True, null=True)
-    date_of_birth = models.DateField(null=True, blank=True)
+    phone_regex = RegexValidator(
+        regex=r'^\+?1?\d{9,15}$',
+        message=_(
+            "Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
+    )
+    phone_number = models.CharField(
+        _('Phone Number'),
+        max_length=15,
+        validators=[phone_regex],
+        blank=True,
+        null=True,
+        help_text=_('Enter your phone number in the format: "+999999999"')
+    )
+    date_of_birth = models.DateField(
+        _('Date of Birth'),
+        null=True,
+        blank=True,
+        help_text=_('Enter your date of birth in the format: YYYY-MM-DD')
+    )
     profile_picture = models.ImageField(
-        upload_to='profile_pictures', blank=True, null=True)
+        _('Profile Picture'),
+        upload_to='profile_pictures',
+        blank=True,
+        null=True,
+        help_text=_('Upload your profile picture')
+    )
 
     def __str__(self):
         """
         Returns a string representation of the user.
         """
-        return self.username
+        return self.get_full_name() or self.username
 
     class Meta:
         """
         Meta class for CustomUser model.
         """
         db_table = 'accounts_customuser'
-        verbose_name = 'User'
-        verbose_name_plural = 'Users'
+        verbose_name = _('User')
+        verbose_name_plural = _('Users')
