0050fbfee97e8851ff24b610a0077b9ff9998fe7
yosephdev <yosephbet@gmail.com>
2024-05-16 10:10:20 +0200

    Import profile_view in accounts/urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,6 @@
 from django.contrib.auth import views as auth_views
 from django.contrib.auth.views import LogoutView
-from .views import signup_view, CustomLoginView
+from .views import signup_view, CustomLoginView, profile_view
 from django.urls import path
 
 app_name = "accounts"
@@ -9,6 +9,7 @@ urlpatterns = [
     path("signup/", signup_view, name="signup"),
     path("login/", CustomLoginView.as_view(), name="login"),
     path("logout/", LogoutView.as_view(next_page="home"), name="logout"),
+    path('profile/', profile_view, name='profile'),
     path(
         "password_reset/", auth_views.PasswordResetView.as_view(), name="password_reset"
     ),
