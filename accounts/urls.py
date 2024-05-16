1262077e4785e3f6325fe740f191b5f62db592f8
yosephdev <yosephbet@gmail.com>
2024-05-16 10:00:28 +0200

    Implement password reset functionality in accounts/urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,11 +1,30 @@
-from django.urls import path
+from django.contrib.auth import views as auth_views
 from django.contrib.auth.views import LogoutView
 from .views import signup_view, CustomLoginView
+from django.urls import path
 
-app_name = 'accounts'
+app_name = "accounts"
 
 urlpatterns = [
-    path('signup/', signup_view, name='signup'),
-    path('login/', CustomLoginView.as_view(), name='login'),
-    path('logout/', LogoutView.as_view(next_page='home'), name='logout'),
+    path("signup/", signup_view, name="signup"),
+    path("login/", CustomLoginView.as_view(), name="login"),
+    path("logout/", LogoutView.as_view(next_page="home"), name="logout"),
+    path(
+        "password_reset/", auth_views.PasswordResetView.as_view(), name="password_reset"
+    ),
+    path(
+        "password_reset/done/",
+        auth_views.PasswordResetDoneView.as_view(),
+        name="password_reset_done",
+    ),
+    path(
+        "reset/<uidb64>/<token>/",
+        auth_views.PasswordResetConfirmView.as_view(),
+        name="password_reset_confirm",
+    ),
+    path(
+        "reset/done/",
+        auth_views.PasswordResetCompleteView.as_view(),
+        name="password_reset_complete",
+    ),
 ]
