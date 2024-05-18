790c860fe760e82105efa2ff89d8eb9de3c683f3
yosephdev <yosephbet@gmail.com>
2024-05-18 09:20:53 +0200

    Add  field to Restaurant model for integration with Google Places API

--- booking_system/models.py
+++ booking_system/models.py
@@ -18,6 +18,7 @@ class Restaurant(models.Model):
     phone_number = models.CharField(max_length=20, blank=True, null=True)
     website = models.URLField(blank=True, null=True)
     rating = models.FloatField(default=0.0)
+    place_id = models.CharField(max_length=255, unique=True)
 
     def __str__(self):
         return self.name
