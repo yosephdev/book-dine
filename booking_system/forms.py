98e754334792de993dc47d02c96d149314ef12f5
yosephdev <yosephbet@gmail.com>
2024-05-19 15:05:41 +0200

    Update ReservationForm in booking_system/forms.py

--- booking_system/forms.py
+++ booking_system/forms.py
@@ -1,11 +1,18 @@
 from django import forms
-from .models import Reservation, Review
+from .models import Reservation, Review, Table
 
 
 class ReservationForm(forms.ModelForm):
+    table = forms.ModelChoiceField(queryset=Table.objects.filter(status='available'))
+
     class Meta:
         model = Reservation
-        fields = ['restaurant', 'date', 'time', 'number_of_guests', 'dietary_restrictions', 'childs_chair', 'special_requests']
+        fields = ['table', 'number_of_guests', 'date', 'time', 'dietary_restrictions', 'childs_chair', 'special_requests']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['table'].queryset = Table.objects.filter(status='available')
+   
 
 class ReviewForm(forms.ModelForm):
     class Meta:
