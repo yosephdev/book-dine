057d750dfafad2dc4fe3645575066268c1d7f86c
yosephdev <yosephbet@gmail.com>
2025-07-24 16:42:11 +0200

    REVERT: wsgi.py to standard config

--- BookDine/wsgi.py
+++ BookDine/wsgi.py
@@ -1,10 +1,14 @@
 """
 WSGI config for BookDine project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 import os
 from django.core.wsgi import get_wsgi_application
 
-# Use production settings for Heroku
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'BookDine.settings.heroku')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'BookDine.settings.development')
 
 application = get_wsgi_application()
